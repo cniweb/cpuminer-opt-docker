8333456d099ec738a17cb265bf6d63df9afb7cb1
Christian Häussler <c-n-i@web.de>
2021-06-11 15:41:18 +0200

    Refactor build.sh
    Signed-off-by: Christian Häussler <c-n-i@web.de>

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
-docker build . --tag cniweb/cpuminer-opt:3.16.2
-#docker tag cniweb/cpuminer-opt:3.16.2 cniweb/cpuminer-opt:latest
-#docker push cniweb/cpuminer-opt:3.16.2
-#docker push cniweb/cpuminer-opt:latest
-docker tag cniweb/cpuminer-opt:3.16.2 ghcr.io/cniweb/cpuminer-opt:3.16.2
-docker tag cniweb/cpuminer-opt:3.16.2 ghcr.io/cniweb/cpuminer-opt:latest
-docker push ghcr.io/cniweb/cpuminer-opt:3.16.2
-docker push ghcr.io/cniweb/cpuminer-opt:latest
+version="3.16.3"
+image="cpuminer-opt"
+docker build . --tag docker.io/cniweb/$image:$version
+docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:$version
+docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:latest
+docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
+docker push ghcr.io/cniweb/$image:$version
+docker push ghcr.io/cniweb/$image:latest
+docker push docker.io/cniweb/$image:$version
+docker push docker.io/cniweb/$image:latest
