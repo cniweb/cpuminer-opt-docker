e8de4448001213108316c7bdc5ba2ea9d13c0c62
Christian Häussler <c-n-i@web.de>
2022-08-17 22:11:43 +0200

    Update to Version 3.20.2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="3.20.0"
+version="3.20.2"
 image="cpuminer-opt"
 docker build . --tag docker.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
