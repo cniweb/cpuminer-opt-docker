3aa1395b2046d60b77de92959df0ce72ae067c53
Christian Häussler <c-n-i@web.de>
2024-05-15 01:01:16 +0200

    Bump to version 24.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="23.15"
+version="24.1"
 image="cpuminer-opt"
 docker build . --tag docker.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
